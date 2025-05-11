f9a076f86e9d350ef6c9685467255a0cabb60414
Trung Nguyen <ductrung.ftu@gmail.com>
2025-05-11 23:24:56 +0700

    Init cars module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { CarsModule } from './cars/cars.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    AuthModule,
+    CarsModule,
+  ],
 })
 export class AppModule {}
