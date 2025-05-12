4d7d71d4344d084ebca8b24bf552025d2561f643
Trung Nguyen <ductrung.ftu@gmail.com>
2025-05-12 16:46:51 +0700

    Create car and UT

--- api/src/cars/cars.service.ts
+++ api/src/cars/cars.service.ts
@@ -1,12 +1,71 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { CreateCarDto } from './dtos/create-car.dto';
+import { PrismaService } from '@/prisma/prisma.service';
 
 @Injectable()
 export class CarsService {
+  constructor(private readonly prismaService: PrismaService) {}
+
   async getAll() {}
 
-  async create() {}
+  async create(createCarDto: CreateCarDto) {
+    // Get the car by plateNumber, throw error if it exists
+    const car = await this.getCarByPlateNumber(createCarDto.plateNumber);
+    if (car) {
+      throw new BadRequestException('Car already exists.');
+    }
+
+    try {
+      // Get the car model and brand name, create a new if that model doesn't exist
+      // Create new car with the modelId from previous step
+      // Both of these steps could be done using the connectOrCreate offered by prisma
+      const createdCar = await this.prismaService.car.create({
+        data: {
+          plateNumber: createCarDto.plateNumber,
+          model: {
+            connectOrCreate: {
+              where: {
+                brand_model: {
+                  brand: createCarDto.brand,
+                  model: createCarDto.model,
+                },
+              },
+              create: {
+                brand: createCarDto.brand,
+                model: createCarDto.model,
+                stock: createCarDto.stock,
+                peakSeasonPrice: createCarDto.peakSeasonPrice,
+                midSeasonPrice: createCarDto.midSeasonPrice,
+                offSeasonPrice: createCarDto.offSeasonPrice,
+                gracePeriod: createCarDto.gracePeriod,
+              },
+            },
+          },
+        },
+        include: {
+          model: true,
+        },
+      });
+
+      return createdCar;
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
+  }
 
   async update() {}
 
   async delete() {}
+
+  getCarByPlateNumber(plateNumber: string) {
+    return this.prismaService.car.findFirst({
+      where: {
+        plateNumber,
+      },
+    });
+  }
 }
